ef877b39d60a57cf9d8088a4cc58030296322d22
pujilesmana <muhammadpuji63@gmail.com>
2021-09-16 14:53:54 +0700

    Nice A

--- main.go
+++ main.go
@@ -6,15 +6,9 @@ func minus(a, b int) int {
 	return a - b
 }
 
-func plus() {
-
-	fmt.Println("nice")
-}
-
 func main() {
 	fmt.Println("Plus")
 	hasil2 := minus(2, 3)
-	plus()
-	fmt.Printf("%d", hasil)
+	fmt.Println("nice")
 	fmt.Printf("%d", hasil2)
 }
